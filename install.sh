0eb59c86da6342e4379db3c87e214a0c2851eee2
maks <maks@pop-os.localdomain>
2022-02-02 16:41:57 +0200

    update install flow, add env folder functional that up customize level

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+bold=$(tput bold)
+folder=$PS_FLOW_FOLDER
 
-sudo cp ./add.sh /usr/bin/ps-add
-sudo cp ./create.sh /usr/bin/ps-create
+if [ -z "$folder" ]; then
+    echo "${red}\$PS_FLOW_FOLDER is empty."
+    echo "Please set \$PS_FLOW_FOLDER as env variable."
+    echo "${green}export PS_FLOW_FOLDER=<path>${reset}"
+else
+    $(createFolderIfNotExist $folder)
+    $(copy)
+    echo "Passwords folder: ${green}${bold} $folder ${reset}"
+fi
+
+function createFolderIfNotExist() {
+    if [ ! -d "$1" ]; then
+        mkdir $1
+    fi
+}
+
+function copy() {
+    sudo cp ./add.sh /usr/bin/ps-add
+    sudo cp ./create.sh /usr/bin/ps-create
+}
